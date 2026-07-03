ff2fb6012fcd63683e5ba68b1eecffcc21ce3e5e
Troels Henriksen <athas@sigkill.dk>
2026-07-03 20:04:58 +0200

    More general wrapper.

--- examples/basic0_scaffold.c
+++ examples/basic0_scaffold.c
@@ -15,6 +15,13 @@ extern void entry_main(MemRef2xI32 *result);
 int main(void) {
   MemRef2xI32 result;
   entry_main(&result);
-  printf("[%d, %d]\n", result.aligned[0], result.aligned[1]);
+  printf("[");
+  for (int64_t i = 0; i < result.sizes[0]; i++) {
+    if (i != 0) {
+      printf(", ");
+    }
+    printf("%d", result.aligned[i]);
+  }
+  printf("]\n");
   return 0;
 }
